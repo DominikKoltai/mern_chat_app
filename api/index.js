17dc860c3c638cbe4e8f9812854125f7485d7396
DominikKoltai <dominik.koltai.28@gmail.com>
2023-07-09 08:25:35 +0200

    API improvement

--- api/index.js
+++ api/index.js
@@ -22,8 +22,33 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
 }));
 
+async function getUserDataFromRequest(req) {
+    return new Promise((resolve, reject) => {
+        const token = req.cookies?.token;
+        if (token) {
+            jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                if (err) throw err;
+                resolve(userData);
+            });
+        } else {
+            reject('no token');
+        }
+    });
+}
+
 app.get('/test', (req, res) => {
     res.json('test ok');
+});
+
+app.get('/messages/:userId', async (req, res) => {
+    const {userId} = req.params;
+    const userData = await getUserDataFromRequest(req);
+    const ourUserId = userData.userId;
+    const messages = await Message.find({
+        sender: {$in: [userId, ourUserId]},
+        recipient: {$in: [userId, ourUserId]},
+    }).sort({createdAt: 1});
+    res.json(messages);
 });
 
 app.get('/profile', (req, res) => {
@@ -96,7 +121,7 @@ wss.on('connection', (connection, req) => {
                 text,
             });
             [...wss.clients].filter(c => c.userId === recipient)
-            .forEach(c => c.send(JSON.stringify({text, sender: connection.userId, recipient, id: messageDoc._id})));
+            .forEach(c => c.send(JSON.stringify({text, sender: connection.userId, recipient, _id: messageDoc._id})));
         }
     });
 
