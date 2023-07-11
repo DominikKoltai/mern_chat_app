2703d62af863d8f155f935944c7c7ae1649c8eb3
DominikKoltai <dominik.koltai.28@gmail.com>
2023-07-11 10:24:45 +0200

    Kubernetes pods, deployments, services added

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -26,12 +26,6 @@ resource "aws_security_group" "private_ssh" {
   vpc_id = aws_vpc.chat_app_vpc.id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_allow_ssh.id]
-  }
-  ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
@@ -43,6 +37,12 @@ resource "aws_security_group" "private_ssh" {
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion_allow_ssh.id]
   }
+  ingress { 
+   		from_port   = 8080 
+        to_port     = 8080
+        protocol    = "tcp" 
+        cidr_blocks = ["0.0.0.0/0"] 
+  } 
   egress {
     from_port   = 0
     to_port     = 0
